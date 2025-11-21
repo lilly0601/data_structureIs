b491e3e99e2b5051d8af0a5084e2a1f5a21eb3fe
nurlanulybeka91 <nurlanulybeka91@gmail.com>
2025-11-21 15:34:08 +0600

    1-tapsyrma-bakylau.py

--- option4/1-tapsyrma-bakylau.py
+++ option4/1-tapsyrma-bakylau.py
@@ -1,19 +1,1 @@
-l1 = [1, 2, 2, 3]
-l2 = [2, 3, 3, 3, 4]
 
-h = {}
-
-for x in l1:
-    h[x] = h.get(x, [0, 0])
-    h[x][0] += 1
-
-for x in l2:
-    h[x] = h.get(x, [0, 0])
-    h[x][1] += 1
-
-res = {}
-
-for x, (a, b) in h.items():
-    res[x] = a*2 + b*3
-
-print(res)
