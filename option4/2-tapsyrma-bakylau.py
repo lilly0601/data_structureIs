6e430754c428a610b8f1719ca97ec7bea3d9f935
nurlanulybeka91 <nurlanulybeka91@gmail.com>
2025-11-21 15:34:21 +0600

    2-tapsyrma-bakylau.py

--- option4/2-tapsyrma-bakylau.py
+++ option4/2-tapsyrma-bakylau.py
@@ -1,12 +1,1 @@
-lst = [3, 7, 11, 15]
 
-if len(lst) < 3:
-    print(True)
-else:
-    d = lst[1] - lst[0]
-    ok = True
-    for i in range(2, len(lst)):
-        if lst[i] - lst[i-1] != d:
-            ok = False
-            break
-    print(ok) 
