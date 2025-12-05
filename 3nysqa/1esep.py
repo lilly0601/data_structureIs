5edbd9ccefd81777f8f0511ac354deffbe7fb61d
Kasym <kasymtorgaev289@gmail.com>
2025-12-05 13:10:35 +0600

    бақылау 3 нұсқа

--- 3nysqa/1esep.py
+++ 3nysqa/1esep.py
@@ -1,29 +1,15 @@
-def iterative_dfs(numbers, graph):
-    visited = [False] * (numbers + 1)
-    count = 0
+n = 6
+ribs = [(1,2),(2,6),(2,3),(3,4)]
 
-    for start in range(1, numbers + 1):
-        if visited[start]:
-            continue
+graph = {str(i): [] for i in range(1, n + 1)}
 
-        stack = [start]
-        visited[start] = True
-
-        while stack:
-            u = stack.pop()
-            for v in graph[u]:
-                if v % 2 == 0:
-                    count += 1
-                if not visited[v]:
-                    visited[v] = True
-                    stack.append(v)
-
-    return count
-
-n = 5
-ribs = [(1,2),(1,3),(2,4),(3,5)]
-graph = [[] for _ in range(n + 1)]
 for a, b in ribs:
-    graph[a].append(b)
+    graph[str(a)].append(b)
 
-print(iterative_dfs(n, graph))
+print("Граф:", graph)
+
+res = [2, 3, 4]
+
+even_count = sum(1 for x in res if x % 2 == 0)
+
+print("Чётные числа в res:", even_count)
